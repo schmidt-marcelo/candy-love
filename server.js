8e821307c04e744b0bf50aa08f6419913b32abd9
Marcelo Schmidt <mschmidt@thoughtworks.com>
2019-05-14 16:41:12 -0400

    Creating a basic server to provide the page

---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/': 
+		fs.readFile(__dirname + "color.html", function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            }); 
+	    break;
+	default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;
+	}
+});
+
+server.listen(1212); 
+
